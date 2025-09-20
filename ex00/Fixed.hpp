e4334c31fb78eed18bea4e8b1bf1a5b08894b964
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-09-20 18:13:31 +0100

    add need files

---
+++ ex00/Fixed.hpp
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/20 17:27:43 by mdahani           #+#    #+#             */
+/*   Updated: 2025/09/20 17:27:43 by mdahani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+// * Includes
+#include <iostream>
+
+// * Classes With Orthodox Canonical Form
+class Fixed{
+    // ! private
+    private:
+        int value;
+        static const int fractionalBits = 8;
+
+    // ! public
+    public:
+        // * Default constructor
+        Fixed();
+
+        // * Copy constructor
+        Fixed(const Fixed &other);
+
+        // * Copy assignment operator
+        Fixed &operator = (const Fixed &other); 
+
+        // * Destructor
+        ~Fixed();
+
+        // * Setters & Getters
+        void setRawBits(int value);
+        int getRawBits();
+};
+
+#endif
