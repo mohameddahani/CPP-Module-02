c4069699ed4302e82b64793689b68f33e66e5f85
Mohamed Dahani <mohamedxdahani@gmail.com>
2025-10-09 15:05:26 +0100

    start on project

---
+++ ex03/Fixed.cpp
@@ -0,0 +1,140 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.cpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/20 17:27:41 by mdahani           #+#    #+#             */
+/*   Updated: 2025/10/09 14:53:45 by mdahani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+// ! Definitions of Constructor, Destructor, Member functions, and Setters, Getters
+
+// * Default constructor with initializer list
+Fixed::Fixed(): fixedPointNumber(0){}
+
+// ! (1 << this->fractionalBits) = 2^8 = 256
+Fixed::Fixed(const int number) : fixedPointNumber(number * (1 << this->fractionalBits)){}
+
+Fixed::Fixed(const float number) : fixedPointNumber(roundf(number * (1 << this->fractionalBits))){}
+
+// * Copy constructor with initializer list
+Fixed::Fixed(const Fixed &other) : fixedPointNumber(other.fixedPointNumber){}
+
+// * Copy assignment operator
+Fixed &Fixed::operator=(const Fixed &other){
+    this->fixedPointNumber = other.fixedPointNumber;
+    return *this;
+}
+
+// * Destructor
+Fixed::~Fixed(){}
+
+// * Setters & Getters
+void Fixed::setRawBits(const int value){
+    this->fixedPointNumber = value;
+}
+
+int Fixed::getRawBits() const {
+    return this->fixedPointNumber;
+}
+
+// * Methods
+int Fixed::toInt() const {
+    return this->fixedPointNumber / (1 << this->fractionalBits);
+}
+
+float Fixed::toFloat() const {
+    return (float)this->fixedPointNumber / (1 << this->fractionalBits);
+}
+
+
+// * (+ - * /) Operator
+// ! Caller --> (Operator +-*/) <-- Input: so the caller call the operator and the operator needs the input inside it so you need to add input on parameter
+Fixed Fixed::operator+(const Fixed &other) const {
+    Fixed result(this->toFloat() + other.toFloat());
+    return result;
+}
+
+Fixed Fixed::operator-(const Fixed &other) const {
+    Fixed result(this->toFloat() - other.toFloat());
+    return result;
+}
+
+Fixed Fixed::operator*(const Fixed &other) const {
+    Fixed result(this->toFloat() * other.toFloat());
+    return result;
+}
+
+Fixed Fixed::operator/(const Fixed &other) const {
+    Fixed result(this->toFloat() / other.toFloat());
+    return result;
+}
+
+// * (pre ++ post ++ & pre -- post --) Operator
+// ! if you add int in parameter like you tell the compailer is post not pre
+Fixed &Fixed::operator++(){
+    this->fixedPointNumber++;
+    return *this;
+}
+Fixed &Fixed::operator--(){
+    this->fixedPointNumber--;
+    return *this;
+}
+
+Fixed Fixed::operator++(int){
+    Fixed old = *this;
+    this->fixedPointNumber++;
+    return old;
+}
+
+Fixed Fixed::operator--(int){
+    Fixed old = *this;
+    this->fixedPointNumber--;
+    return old;
+}
+
+
+// * Operator << insertio
+// ! Overload = defining a function or an operator with the same name but with different parameters.
+// ! The compiler automatically chooses the appropriate definition based on the type of the data.
+std::ostream &operator<<(std::ostream &output, const Fixed &other){
+    return output << other.toFloat();
+}
+
+ // * max and min
+Fixed &Fixed::min(Fixed &obj1, Fixed &obj2){
+    if (obj1.toFloat() > obj2.toFloat()){
+        return obj2;
+    } else {
+        return obj1;
+    }
+}
+
+const Fixed &Fixed::min(const Fixed &obj1, const Fixed &obj2){
+    if (obj1.toFloat() > obj2.toFloat()){
+        return obj2;
+    } else {
+        return obj1;
+    }
+}
+
+Fixed &Fixed::max(Fixed &obj1, Fixed &obj2){
+    if (obj1.toFloat() > obj2.toFloat()){
+        return obj1;
+    } else {
+        return obj2;
+    }
+}
+
+const Fixed &Fixed::max(const Fixed &obj1, const Fixed &obj2){
+    if (obj1.toFloat() > obj2.toFloat()){
+        return obj1;
+    } else {
+        return obj2;
+    }
+}
---
+++ ex03/Fixed.hpp
@@ -0,0 +1,74 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Fixed.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mdahani <mdahani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/20 17:27:43 by mdahani           #+#    #+#             */
+/*   Updated: 2025/10/09 14:49:45 by mdahani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FIXED_HPP
+# define FIXED_HPP
+
+// * Includes
+#include <iostream>
+#include <cmath>
+
+// * Classes With Orthodox Canonical Form
+class Fixed{
+    // ! private
+    private:
+        int fixedPointNumber;
+        static const int fractionalBits = 8;
+
+    // ! public
+    public:
+        // * Default constructor
+        Fixed();
+
+        Fixed(const int number);
+        Fixed(const float number);
+
+        // * Copy constructor
+        Fixed(const Fixed &other);
+
+        // * Copy assignment operator
+        Fixed &operator=(const Fixed &other);
+
+        // * Destructor
+        ~Fixed();
+
+        // * Setters & Getters
+        void setRawBits(const int value);
+        int getRawBits() const;
+
+        // * Methods
+        float toFloat() const;
+        int toInt() const;
+
+        // * (+ - * /) Operator
+        Fixed operator+(const Fixed &other) const;
+        Fixed operator-(const Fixed &other) const;
+        Fixed operator*(const Fixed &other) const;
+        Fixed operator/(const Fixed &other) const;
+
+        // * (pre ++ post ++ & pre -- post --) Operator
+        Fixed &operator++();
+        Fixed &operator--();
+        Fixed operator++(int);
+        Fixed operator--(int);
+
+        // * max and min
+        static Fixed &min(Fixed &obj1, Fixed &obj2);
+        static const Fixed &min(const Fixed &obj1, const Fixed &obj2);
+        
+        static Fixed &max(Fixed &obj1, Fixed &obj2);
+        static const Fixed &max(const Fixed &obj1, const Fixed &obj2);
+};
+
+std::ostream &operator<<(std::ostream &output, const Fixed &other);
+
+#endif
